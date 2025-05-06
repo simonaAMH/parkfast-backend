5a38def18c2b4baf4e5c1e8fb27c7512f34ecfa1
simonaAMH <amihaesiisimona5@gmail.com>
2025-05-06 22:31:24 +0300

    fix

--- src/main/java/com/example/licenta/Config/DotenvConfig.java
+++ src/main/java/com/example/licenta/Config/DotenvConfig.java
@@ -3,13 +3,15 @@ package com.example.licenta.Config;
 import io.github.cdimascio.dotenv.Dotenv;
 import jakarta.annotation.PostConstruct;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
+@Profile("local")
 public class DotenvConfig {
 
     @PostConstruct
     public void loadEnv() {
-        Dotenv dotenv = Dotenv.configure().load();
+        Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
 
         dotenv.entries().forEach(entry -> {
             System.setProperty(entry.getKey(), entry.getValue());
