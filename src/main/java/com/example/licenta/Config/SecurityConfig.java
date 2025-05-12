bf25ccf0084092dcc912eac1217d91d01861bc74
simonaAMH <amihaesiisimona5@gmail.com>
2025-05-12 16:39:12 +0300

    allow no security for the deep linking file

--- src/main/java/com/example/licenta/Config/SecurityConfig.java
+++ src/main/java/com/example/licenta/Config/SecurityConfig.java
@@ -73,6 +73,7 @@ public class SecurityConfig {
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
+                                "/.well-known/**",
                                 "/api/users/register",
                                 "/api/users/login",
                                 "/api/users/verify-email",
