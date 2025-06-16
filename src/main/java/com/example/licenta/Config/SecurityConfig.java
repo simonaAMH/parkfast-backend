463fbc38267762d767ec2d5bbb64157ff3f7c45b
simonaAMH <amihaesiisimona5@gmail.com>
2025-06-16 23:47:45 +0300

    stripe fix

--- src/main/java/com/example/licenta/Config/SecurityConfig.java
+++ src/main/java/com/example/licenta/Config/SecurityConfig.java
@@ -119,8 +119,7 @@ public class SecurityConfig {
                                 "/api-docs/**",
                                 "/swagger-resources/**",
                                 "/webjars/**",
-                                "/api/webhooks/stripe",
-                                "/api/webhooks/stripe/**"
+                                "/api/webhooks/**"
                         ).permitAll()
 
                         .anyRequest().authenticated());
